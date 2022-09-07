e95d92e55d15d44aaaf7a6ce4a8ad19a5432f77e
neupanerabin <65486178+neupanerabin@users.noreply.github.com>
2022-09-07 19:08:17 +0545

    Update info.py

--- Django/login_Console/practice1/practice1/info.py
+++ Django/login_Console/practice1/practice1/info.py
@@ -1,7 +1,7 @@
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'rabinneupane07@gmail.com'
-EMAIL_HOST_PASSWORD = 'Passwords@1'
+EMAIL_HOST_USER = '____________'  # Put your id on here
+EMAIL_HOST_PASSWORD = '_____'     # Put your passwords here
 EMAIL_PORT = 587
 
 
